033e81227242e5a83de668bc45064a00024623ce
Daniele De Sano <daniele.desano@gmail.com>
2018-12-29 00:25:38 +0100

    Add files via upload

---
+++ nano_test_03.ino
@@ -0,0 +1,19 @@
+int ledPinR1 = 10;
+int ledPinY1 = 9;
+int ledPinR2 = 11;
+
+void setup()
+{
+pinMode(ledPinR1, OUTPUT);
+pinMode(ledPinY1, OUTPUT);
+pinMode(ledPinR2, OUTPUT);
+}
+
+void loop() {
+analogWrite(ledPinR1, random(10,200));
+delay(random(50));
+analogWrite(ledPinY1, random(10,200));
+delay(random(50));
+analogWrite(ledPinR2, random(10,200));
+delay(random(50));
+}
